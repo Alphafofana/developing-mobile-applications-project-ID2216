9ddaf55fce9b5aa8442daaf577ca5d111d844455
nedvin <in.nivde@gmail.com>
2021-02-16 14:40:12 +0100

    Create navigation bar component

---
+++ quiz-royale-native-app/src/colors.js
@@ -0,0 +1,9 @@
+const Colors = {
+	LIGHT_BLUE: "#9BA4B4",
+	MIDDLE_BLUE: "#394867",
+	DARK_BLUE: "#14274E",
+	GOLD: "#CEAD02",
+	WHITE: "#F1F6F9",
+};
+
+export default Colors;
---
+++ quiz-royale-native-app/src/components/NavigationBar.js
@@ -0,0 +1,38 @@
+import React from "react";
+import { View, TouchableOpacity, StyleSheet } from "react-native";
+import { FontAwesome, FontAwesome5 } from "@expo/vector-icons";
+import Colors from "../colors";
+
+function NavigationBar() {
+	return (
+		<View style={styles.view}>
+			<TouchableOpacity>
+				<FontAwesome size={50} name="home" color={Colors.WHITE} />
+			</TouchableOpacity>
+			<TouchableOpacity>
+				<FontAwesome size={50} name="user" color={Colors.WHITE} />
+			</TouchableOpacity>
+			<TouchableOpacity>
+				<FontAwesome5
+					size={50}
+					name="chart-line"
+					color={Colors.WHITE}
+				/>
+			</TouchableOpacity>
+		</View>
+	);
+}
+
+const styles = StyleSheet.create({
+	view: {
+		backgroundColor: Colors.DARK_BLUE,
+		paddingTop: 20,
+		paddingBottom: 20,
+		color: Colors.WHITE,
+		flexDirection: "row",
+		alignSelf: "stretch",
+		justifyContent: "space-around",
+	},
+});
+
+export default NavigationBar;
