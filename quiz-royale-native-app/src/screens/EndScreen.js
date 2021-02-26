096d2236def3aefc54ee86e3bc6ee80754ffa4b7
nedvin <in.nivde@gmail.com>
2021-02-26 08:06:06 +0100

    Bug fix

--- quiz-royale-native-app/src/screens/EndScreen.js
+++ quiz-royale-native-app/src/screens/EndScreen.js
@@ -4,30 +4,45 @@ import Colors from "../colors";
 import Header from "../components/Header";
 import NavigationBar from "../components/NavigationBar";
 
-export default function EndScreen({navigation, userHasWon}) {
+export default function EndScreen({ navigation, userHasWon }) {
 	return (
 		<>
-            <Header/>
+			<Header />
 			<View style={styles.main}>
-                <Text style={{fontSize: 24, color: 'white'}}>{userHasWon ? "You Win" : "Rillmeister Wins"}</Text>
-                <Text style={{fontSize: 18, color: 'white'}}>Username of the winner</Text>
-                <br></br>
-                <View style={{flex: 1, justifyContent: "space-between"}}>
-                    <View style={StyleSheet.mainmiddlescreen}>
-                        <Text style={{fontSize: 18, color: 'white', fontWeight: 'bold'}}>Eliminated</Text>
-                        <View style={styles.line}></View>
-                        <Text style={{fontSize: 18, color: 'white'}}>{userHasWon ? "Rillmeister" : "User"}</Text>
-                    </View>
-                    <TouchableOpacity style={styles.button} onPress={() => navigation.navigate("LobbyScreen")}>
-                        <View style={styles.center}>
-                            <Text style={{fontSize: 18}}>Back to Lobby</Text>
-                        </View>
-                    </TouchableOpacity>
-                </View>
+				<Text style={{ fontSize: 24, color: "white" }}>
+					{userHasWon ? "You Win" : "Rillmeister Wins"}
+				</Text>
+				<Text style={{ fontSize: 18, color: "white" }}>
+					Username of the winner
+				</Text>
+
+				<View style={{ flex: 1, justifyContent: "space-between" }}>
+					<View style={StyleSheet.mainmiddlescreen}>
+						<Text
+							style={{
+								fontSize: 18,
+								color: "white",
+								fontWeight: "bold",
+							}}
+						>
+							Eliminated
+						</Text>
+						<View style={styles.line}></View>
+						<Text style={{ fontSize: 18, color: "white" }}>
+							{userHasWon ? "Rillmeister" : "User"}
+						</Text>
+					</View>
+					<TouchableOpacity
+						style={styles.button}
+						onPress={() => navigation.navigate("LobbyScreen")}
+					>
+						<View style={styles.center}>
+							<Text style={{ fontSize: 18 }}>Back to Lobby</Text>
+						</View>
+					</TouchableOpacity>
+				</View>
 			</View>
-			<NavigationBar
-                navigation={navigation}
-            />
+			<NavigationBar navigation={navigation} />
 		</>
 	);
 }
@@ -45,21 +60,20 @@ const styles = StyleSheet.create({
 		height: 50,
 		borderRadius: 40,
 		margin: 5,
-        marginBottom: 20,
+		marginBottom: 20,
 	},
 	center: {
 		flex: 1,
 		alignItems: "center",
 		justifyContent: "center",
 	},
-    mainmiddlescreen: {
-        flex: 1,
-        textAlign: "left",
-    },
-    line: {
-        borderBottomColor: "white",
-        borderBottomWidth: 2,
-        width: 280,
-    }
+	mainmiddlescreen: {
+		flex: 1,
+		textAlign: "left",
+	},
+	line: {
+		borderBottomColor: "white",
+		borderBottomWidth: 2,
+		width: 280,
+	},
 });
-
